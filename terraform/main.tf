627f5da1f5833ff6a142ee142b70dba55c187061
Dusan <dusan@me.com>
2025-07-08 14:57:40 +0200

    added node identity

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
+    node_identity_type = "SystemAssigned"
   }
 
   identity {
